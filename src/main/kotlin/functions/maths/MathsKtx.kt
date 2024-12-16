8696ecb52eba54f4c15df1de891241720ca569fd
ch8n <chetan.garg36@gmail.com>
2024-12-16 23:40:51 +0530

    wip agentik tools

---
+++ src/main/kotlin/functions/maths/MathsKtx.kt
@@ -0,0 +1,55 @@
+package functions.maths
+
+import dev.langchain4j.agent.tool.Tool
+import kotlin.math.*
+
+
+class MathsKtx {
+    @Tool("Returns the absolute value of a number")
+    fun absoluteValueOfNumber(number: Double): Double {
+        return abs(number)
+    }
+
+    @Tool("Returns minimum from list of numbers")
+    fun minimumOf(vararg number: Double): Double {
+        val take1 = number.first()
+        val rest = number.drop(1)
+        return minOf(take1, *rest.toTypedArray())
+    }
+
+    @Tool("Returns maximum from list of numbers")
+    fun maximumOf(vararg number: Double): Double {
+        val take1 = number.first()
+        val rest = number.drop(1)
+        return maxOf(take1, *rest.toTypedArray())
+    }
+
+    @Tool("Computes the square root of a number")
+    fun squareRootOf(number: Double): Double = sqrt(number)
+
+    @Tool("Raises a number to the power of another number")
+    fun powerOf(number: Double, power: Double): Double = number.pow(power)
+
+    @Tool("Round a floating point number to nearest integer, example: 2.5 to 3.0")
+    fun roundOff(number: Double) = round(number)
+
+    @Tool("Floor a floating point number to down integer, example: 2.9 to 2.0")
+    fun floorOf(number: Double) = floor(number)
+
+    @Tool("Ceil a floating point number to nearest integer, example: 2.1 to 3.0")
+    fun cielOf(number: Double) = ceil(number)
+
+    @Tool("Returns sum from list of numbers")
+    fun sumOf(vararg number: Double): Double = number.sum()
+
+    @Tool("Returns minus from list of numbers")
+    fun minusOf(vararg number: Double): Double = number.reduce { acc, it -> acc - it }
+
+    @Tool("Returns product from list of numbers")
+    fun productOf(vararg number: Double): Double = number.reduce { acc, it -> acc * it }
+
+    @Tool("Returns division of two numbers")
+    fun productOf(number1: Double, number2: Double): Double = number1.div(number2)
+}
+
+
---
+++ src/main/kotlin/functions/websearch/WebSearchKtx.kt
@@ -0,0 +1,5 @@
+package functions.websearch
+
+class WebSearchKtx {
+    
+}
