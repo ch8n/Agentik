ef4b03adadf503526ad567d4e97f2bd63ad1f0f4
ch8n <chetan.garg36@gmail.com>
2025-01-25 11:11:26 +0530

    added role based agent

--- src/main/kotlin/agent/RoleBasedAgent.kt
+++ src/main/kotlin/agent/RoleBasedAgent.kt
@@ -1,62 +1,55 @@
 package agent
 
+import agent.planningAgent.AgentikAgent
 import models.AgentikModel
-import opennlp.tools.util.model.ModelType
-
-class MultiStepAgent
 
 object AgentDefaults {
     val modelName: String = "llama3.2:latest"
     val modelType: AgentikModel = AgentikModel.Ollama
 }
 
-data class ManagedAgent(
-    val agentHumanName: String,
-    val additionalPrompting: String? = null,
+data class RoleBasedAgent(
+    override val name: String,
+    override val description: String,
     val modelType: AgentikModel = AgentDefaults.modelType,
     val modelName: String = AgentDefaults.modelName,
-    val tools : List<AgentikTool> = emptyList()
-) {
+    val tools: List<AgentikTool> = emptyList(),
+) : AgentikAgent {
+
     private val agent = Agentik(
         modelType = modelType,
         modelName = modelName,
         tools = tools
     )
 
-    fun getTaskSummary(request: String): String {
-        val output = execute(request)
+    fun execute(task: String): String {
+        val prompt = ManagedAgentPrompts.AGENT_PROMPT(
+            name = name, task = task, description = description
+        )
+        val output = agent.execute(prompt)
         val summary = buildString {
-            appendLine("Here is the final answer from your managed agent '$agentHumanName':")
+            appendLine("Here is the final answer from your managed agent '$name':")
             append(output)
             appendLine()
             appendLine("For more detail, find below a summary of this agent's work:")
-            appendLine("SUMMARY OF WORK FROM AGENT '$agentHumanName':")
+            appendLine("SUMMARY OF WORK FROM AGENT '$name':")
             for (message in agent.messages()) {
                 appendLine(message)
             }
-            appendLine("END OF SUMMARY OF WORK FROM AGENT '$agentHumanName'.")
+            appendLine("END OF SUMMARY OF WORK FROM AGENT '$name'.")
         }
         return summary
-    }
-
-    fun execute(request: String): String {
-        val prompt = ManagedAgentPrompts.MANAGED_AGENT_PROMPT(
-            name = agentHumanName,
-            task = request,
-            additionalPrompting = additionalPrompting ?: ""
-        )
-        return agent.execute(prompt)
     }
 }
 
 
 object ManagedAgentPrompts {
-    fun MANAGED_AGENT_PROMPT(
+    fun AGENT_PROMPT(
         name: String,
         task: String,
-        additionalPrompting: String
+        description: String
     ) = """
-You're a helpful agent named '${name}'.
+You're a helpful agent named '${name}', $description.
 You have been submitted this task by your manager.
 ---
 Task:
@@ -70,9 +63,7 @@ Your final_answer WILL HAVE to contain these parts:
 ### 2. Task outcome (extremely detailed version):
 ### 3. Additional context (if relevant):
 
-Put all these in your final_answer tool, 
-everything that you do not pass as an argument to final_answer will be lost.
 And even if your task resolution is not successful, 
 please return as much context as possible, so that your manager can act upon this feedback.
-$additionalPrompting"""
+"""
 }
